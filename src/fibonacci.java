7c6a8f1c29f0836dc78d455127c7e6692d95e18f
andrey rojas <andreymrojas668@gmail.com>
2022-07-02 12:28:16 -0500

    subida de mi proyecto

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,2 +1,14 @@
 public class fibonacci {
+    public static void main(String args[]){
+        int a =0;
+        int b =1;
+        int c;
+        for (int i=0 ; i<=30; i++){
+            c=a+b;
+            System.out.println(a);
+            a=b;
+            b=c;
+
+        }
+    }
 }
